852153f429b38fdfeea717d0eac246f8e5ad0c07
Shekhireva <Shehireva.87@yandex.ru>
2024-06-26 23:07:54 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+first = 12
+second = 16
+third = 26
+if first == second and second == third and third == first:
+    print(3)
+elif first != second and second != third and third != first:
+    print(0)
+#else: first == second and
+print(first)
+
